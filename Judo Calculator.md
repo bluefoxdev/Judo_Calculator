e1ebc463a2168d19292e4e673009b5724bdc4acd
bluefoxdev <lewissang123@gmail.com>
2026-03-24 15:58:37 +0000

    Update 1 (unfinished) -get_plan function added to Athlete class -added early .md file -set foundations for gym memberships -fixed some syntax errors and cleaned up code

---
+++ Judo Calculator.md
@@ -0,0 +1,20 @@
+### Judo Calculator
+
+
+
+The judo calculator is a University project for a made up judo gym company, used to calculate the cost to the athletes for the chosen gym plans (subscriptions) as well as putting the athlete into a correct weight class.
+
+
+
+
+
+When the project began the program was given the following requirements to follow:
+
+
+
+* cater to the above project requirements
+* have no branching if statements 
+* use OOP (Object Oriented Programming)
+* demonstrate inheritance 
+* feature at least one pure function
+
--- Main.py
+++ Main.py
@@ -53,17 +53,18 @@ class Athlete(Person):
             self.Age = 0
             self.Weight = 0
             self.Sex = ""
-            self.Weight_category = ""
+            self.Weight_category = ""#
+            self.gym_plan = ""
             #loops until a valid age other than zero entered
             while(self.Age == 0):
                 #ageinput
-                attempt = input('Enter Age(eg "34"):')
+                Attempt = input('Enter Age(eg "34"):')
                 #remove all whitespaces from back and front of input
-                attempt = attempt.strip()
+                Attempt = Attempt.strip()
 
                 #validates to see if input can be converted to an integer
                 try:
-                    self.Age = int(attempt)
+                    self.Age = int(Attempt)
                     #if age is just a number and is above 0, it is valid, else prompt user to try again
                     if self.Age > 0:
                         break
@@ -76,13 +77,13 @@ class Athlete(Person):
             #loops till valid Weight is given
             while(self.Weight == 0):
                 #input
-                attempt = input('Enter Weight in kgs (eg "34.5"):')
+                Attempt = input('Enter Weight in kgs (eg "34.5"):')
                 #remove all whitespaces from back and front of input
-                attempt = attempt.strip()
+                Attempt = Attempt.strip()
 
                 #validates to see if input can be converted to an integer
                 try:
-                    self.Weight = float(attempt)
+                    self.Weight = float(Attempt)
                     #if Weight is just a number and is above 0, it is valid, else prompt user to try again
                     if self.Weight > 0:
                         break
@@ -92,17 +93,23 @@ class Athlete(Person):
                 except ValueError:
                     print('Weight must be given as a number alone (no "kilograms" or "kgs")')
 
-            #
             while(self.Sex == ""):
-                attempt = input('Enter Sex(eg "M" or "F"):')
-                attempt = attempt.strip().upper()
+                Attempt = input('Enter Sex(eg "M" or "F"):')
+                Attempt = Attempt.strip().upper()
 
-                if attempt in ["M", "F"]:
-                    self.Sex = attempt
-                elif input('Unrecognized Sex, would you like to be marked as "other"? Y/N').strip().upper() == "Y":
-                    self.Sex = "O"
+                if Attempt in ["M", "F"]:
+                    self.Sex = Attempt
                 else:
-                    pass
+                    print("Invalid Sex entered, for the Safety of other athletes either M or F needs to be specified for proper weight classification, please enter M or F")
+
+            while(self.gym_plan == ""):
+                Attempt = input('Enter desired plan number\n1:Beginner\n2:Intermediate\n3:Advanced\n4:Professional')
+                Attempt = Attempt.strip()
+
+                if Attempt[0] in ["1", "2", "3", "4"]:
+                    self.gym_plan = Attempt
+                else:
+                    print("Invalid, please enter a number of the desired plan on the list.")
 
         if self.Sex == "M" and self.Weight < 73:
             self.Weight_category = "Male Light Weight"
@@ -121,12 +128,26 @@ class Athlete(Person):
     def introduction(self):
         return f"Hello, my Name is {self.Name}, I am {self.Age} year old {self.get_Sex()} Athlete, and I Weight {self.Weight} kgs,\nthis puts me in the {self.Weight_category} category."
     
+    def get_plan(self):
+        match self.gym_plan:
+            case "1":
+                return "Beginner"
+            case "2":
+                return "Intermediate"
+            case "3":
+                return "Advance"
+            case "4":
+                return "Professional"
+            case _:
+                return f'unrecognised plan number "{self.gym_plan}" for athlete {self.Name}'
+
     def print_info(self):
         print(f"Name: {self.Name}")
         print(f"Age: {self.Age}")
         print(f"Weight: {self.Weight}")
         print(f"Sex: {self.get_Sex()}")
         print(f"Weight category: {self.Weight_category}")
+        print(f"Gym plan: {self.get_plan()}")
         
 
 #returns the list of costs and total costs
@@ -161,7 +182,7 @@ def main():
         print("1: Register Athlete")
         print("2: View Athletes")
         print("3: delete Athlete")
-        print("4: save reciept")
+        print("4: save reciept") #out of scope, remove for submission
         choice = input("Enter service number:")
         choice = choice.strip()
 
@@ -175,19 +196,20 @@ def main():
                     for instance in Athletes:
                         print(f"\nAthlete {Athletes.index(instance)+1}:")
                         Athlete.print_info()
-                        print(f"-Charge: {prices[0]}")
+                        print(f"-Charge: £{prices[1][instance]}")
+                    print(f"\nTotal: £{prices[0]}")
 
                 case "3":
                     for instance in Athletes:
                         print(f"\nAthlete {Athletes.index(instance)+1}:")
                         instance.print_info()
-                    delete_attempt = input("Enter the number of the Athlete you want to delete:")
-                    delete_attempt = delete_attempt.strip()
+                    delete_Attempt = input("Enter the number of the Athlete you want to delete:")
+                    delete_Attempt = delete_Attempt.strip()
 
                     try:
-                        delete_attempt = int(delete_attempt[0])
-                        if delete_attempt > 0 and delete_attempt <= len(Athletes):
-                            del Athletes[delete_attempt-1]
+                        delete_Attempt = int(delete_Attempt[0])
+                        if delete_Attempt > 0 and delete_Attempt <= len(Athletes):
+                            del Athletes[delete_Attempt-1]
                             print("Athlete deleted successfully")
                         else:
                             print("Invalid choice, number out of range")
@@ -216,8 +238,8 @@ def main():
                             print(f"Data saved as Receipt{receipt_number} in\n{storage_directory}\n do not edit the contents to ensure proper loading later on.")
                 
                 case _:
-                    print("Invalid choice")
-        #warn user if no 
+                    print(f"Invalid choice {choice[0]}")
+        #warn user if answer is not numeric
         else:
             print('Answer not Numeric (eg: "1")')
 
