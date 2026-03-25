d868204253341b338214d9a4bfa027b45f223699
bluefoxdev <lewissang123@gmail.com>
2026-03-25 11:17:33 +0000

    update 2 (unfinished) -prices are now included now when viewing athletes -added a way to exit the program from the program -plans and their prices are now stored as a Dictionary so they can be accessed with a string -fixed some grammer -added more comments -bug fixes -delete "Judo Calculator.md" as it is replaced by "README.md" which is the correct format

--- Main.py
+++ Main.py
@@ -12,8 +12,14 @@ Beginner = 25
 Intermediate = 45
 Advanced = 80
 Professional = 120
+
+plans = {
+    "Beginner": 25,
+    "Intermediate": 45,
+    "Advanced": 80,
+    "Professional": 120,
+}
 ########
-###
 
 
 class Person():
@@ -28,7 +34,7 @@ class Person():
     def get_Sex(self):
         if self.Sex == "M":
             return "male"
-        elif self.Sex == "F":
+        else:
             return "female"
     
     #prints an introductory sentance
@@ -55,7 +61,7 @@ class Athlete(Person):
             self.Sex = ""
             self.Weight_category = ""#
             self.gym_plan = ""
-            #loops until a valid age other than zero entered
+            #loops until a valid age other than zero entered by user
             while(self.Age == 0):
                 #ageinput
                 Attempt = input('Enter Age(eg "34"):')
@@ -74,7 +80,7 @@ class Athlete(Person):
                 except ValueError:
                     print('Age must be given as number of years alone (no "year" or "yr"), please retry.')
 
-            #loops till valid Weight is given
+            #loops till valid Weight is given by user
             while(self.Weight == 0):
                 #input
                 Attempt = input('Enter Weight in kgs (eg "34.5"):')
@@ -93,17 +99,19 @@ class Athlete(Person):
                 except ValueError:
                     print('Weight must be given as a number alone (no "kilograms" or "kgs")')
 
+            #loops till valid Sex is given by user
             while(self.Sex == ""):
+                #user input and input formatting
                 Attempt = input('Enter Sex(eg "M" or "F"):')
                 Attempt = Attempt.strip().upper()
-
+                #checks if the input is M or F, if not warn user and allow it to be entered
                 if Attempt in ["M", "F"]:
                     self.Sex = Attempt
                 else:
                     print("Invalid Sex entered, for the Safety of other athletes either M or F needs to be specified for proper weight classification, please enter M or F")
 
             while(self.gym_plan == ""):
-                Attempt = input('Enter desired plan number\n1:Beginner\n2:Intermediate\n3:Advanced\n4:Professional')
+                Attempt = input('Enter desired plan number\n1:Beginner\n2:Intermediate\n3:Advanced\n4:Professional\n')
                 Attempt = Attempt.strip()
 
                 if Attempt[0] in ["1", "2", "3", "4"]:
@@ -111,18 +119,30 @@ class Athlete(Person):
                 else:
                     print("Invalid, please enter a number of the desired plan on the list.")
 
+        #assigns Weight catagory based on weight and sex
+        #Males
         if self.Sex == "M" and self.Weight < 73:
             self.Weight_category = "Male Light Weight"
+        
         elif self.Sex == "M" and self.Weight < 90:
             self.Weight_category = "Male medium Weight"
+        
         elif self.Sex == "M" and self.Weight > 90:
             self.Weight_category = "Male Heavy Weight"
+        #Females
         elif self.Sex == "F" and self.Weight < 57:
             self.Weight_category = "Female Light Weight"
+        
         elif self.Sex == "F" and self.Weight < 70:
             self.Weight_category = "Female medium Weight"
+        
         elif self.Sex == "F" and self.Weight > 70:
             self.Weight_category = "Female Heavy Weight"
+        else:
+            #this is impossible, but for whatever reason it does happen, it will be caught here
+            self.Weight_category = "unassigned"
+            print(f"warning: no parameters reached for placing in a weight catagory with sex: {self.Sex} and weight: {self.Weight}, please inform a member of staff.")
+
         print("Athlete created successfully")
 
     def introduction(self):
@@ -141,6 +161,7 @@ class Athlete(Person):
             case _:
                 return f'unrecognised plan number "{self.gym_plan}" for athlete {self.Name}'
 
+    #print all the info of the athlete
     def print_info(self):
         print(f"Name: {self.Name}")
         print(f"Age: {self.Age}")
@@ -152,37 +173,33 @@ class Athlete(Person):
 
 #returns the list of costs and total costs
 def calculate_cost(Athletes):
-    price = 0
-    prices = []
+    #total price of all athletes plans
+    Price = 0
+    #indivdual prices in order of athletes
+    Prices = []
+    #loop through athletes and assign cost based on chosen plan
     for instance in Athletes:
-        match instance.gym_plan:
-            case "1":
-                price += Beginner
-                prices.append(Beginner)
-            case "2":
-                price += Intermediate
-                prices.append(Intermediate)
-            case "3":
-                price += Advanced
-                prices.append(Advanced)
-            case "4":
-                price += Professional
-                prices.append(Professional)
-            case _:
-                return f'unsupported value given for gym membership: "{Athlete.gym_plan}" please inform a member of staff.'
-    return [price, prices]
+        try:
+            Plan_price = plans[instance.get_plan()]
+        except KeyError:
+            #print error message if unrecognised plan
+            print(instance.get_plan())
+        Price += Plan_price
+        Prices.append(Plan_price)
+    return [Price, Prices]
 
 
 def main():
     loop = True
-
+    #main program loop
     while(loop == True):
         print("##########JUDO APP##########\n")
-        print("please type a number for service")
+        print("Please type a number for service")
         print("1: Register Athlete")
         print("2: View Athletes")
-        print("3: delete Athlete")
-        print("4: save reciept") #out of scope, remove for submission
+        print("3: Delete Athlete")
+        print("4: Save reciept") #out of scope, remove for submission
+        print("5: Exit")
         choice = input("Enter service number:")
         choice = choice.strip()
 
@@ -195,8 +212,8 @@ def main():
                     prices = calculate_cost(Athletes)
                     for instance in Athletes:
                         print(f"\nAthlete {Athletes.index(instance)+1}:")
-                        Athlete.print_info()
-                        print(f"-Charge: £{prices[1][instance]}")
+                        instance.print_info()
+                        print(f"-Charge: £{prices[1][Athletes.index(instance)]}")
                     print(f"\nTotal: £{prices[0]}")
 
                 case "3":
@@ -237,6 +254,9 @@ def main():
                             file.close()
                             print(f"Data saved as Receipt{receipt_number} in\n{storage_directory}\n do not edit the contents to ensure proper loading later on.")
                 
+                case "5":
+                    print("Have a good day!")
+                    loop = False
                 case _:
                     print(f"Invalid choice {choice[0]}")
         #warn user if answer is not numeric
