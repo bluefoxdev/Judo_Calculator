c55d1769163afeb2dbf3a79bd33b30f61dd61653
bluefoxdev <lewissang123@gmail.com>
2026-05-07 10:07:11 +0100

    Patch 1.0.2 Patch
    standardisation patch
    
    -convert all functions to camel_case

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@
 
 ###Global variable###
 #Athlete storage#
-Athletes = []
+athletes = []
 ###
 
 ###Service prices###
@@ -28,33 +28,33 @@ plans = {
 
 class Person():
     #initialisation 
-    def __init__(self, Name, Age, Weight, Sex):
-        self.Name = Name
-        self.Age = Age
-        self.Weight = Weight
-        self.Sex = Sex
+    def __init__(self, name, age, weight, sex):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.sex = sex
 
-    #returns a user friendly version of the Sex for display
-    def get_Sex(self):
-        if self.Sex == "M":
+    #returns a user friendly version of the sex for display
+    def get_sex(self):
+        if self.sex == "M":
             return "male"
         else:
             return "female"
     
     #prints an introductory sentance
     def introduction(self):
-        return f"Hello, my Name is {self.Name}, I am {self.Age} year old {self.get_Sex()}, and I Weight {self.Weight} kgs."
+        return f"Hello, my name is {self.name}, I am {self.age} year old {self.get_sex()}, and I weight {self.weight} kgs."
         
 class Athlete(Person):
     #initialisation
-    def __init__(self, Name, Age, Weight, Sex, gym_plan, competitions, coaching_hours, manual_input):
+    def __init__(self, name, age, weight, sex, gym_plan, competitions, coaching_hours, manual_input):
         #determines if the program or the user is filling initalisation info
         if manual_input == False:
             #program fills out
-            self.Name = Name
-            self.Age = Age
-            self.Weight = Weight
-            self.Sex = Sex
+            self.name = name
+            self.age = age
+            self.weight = weight
+            self.sex = sex
             self.gym_plan = gym_plan
             self.competitions = competitions
             self.coaching_hours = coaching_hours
@@ -63,62 +63,62 @@ class Athlete(Person):
         else:
             #user fills out
             print("#####Adding new Athlete#####\n")
-            self.Name = input("Enter Name:")
-            self.Age = 0
-            self.Weight = 0
-            self.Sex = ""
-            self.Weight_category = ""
+            self.name = input("Enter name:")
+            self.age = 0
+            self.weight = 0
+            self.sex = ""
+            self.weight_category = ""
             self.gym_plan = ""
             self.competitions = ""
             self.coaching_hours = ""
             #loops until a valid age other than zero entered by user
-            while(self.Age == 0):
+            while(self.age == 0):
                 #ageinput
-                Attempt = input('Enter Age(eg "34"):')
+                Attempt = input('Enter age(eg "34"):')
                 #remove all whitespaces from back and front of input
                 Attempt = Attempt.strip()
 
                 #validates to see if input can be converted to an integer
                 try:
-                    self.Age = int(Attempt)
+                    self.age = int(Attempt)
                     #if age is just a number and is above 0, it is valid, else prompt user to try again
-                    if self.Age > 0:
+                    if self.age > 0:
                         break
                     else:
-                        print('Age must be a positive number and more than 0, please retry.')
+                        print('age must be a positive number and more than 0, please retry.')
                 #input contains more than just numbers prompt user to retry
                 except ValueError:
-                    print('Age must be given as number of years alone (no "year" or "yr" and no decimals), please retry.')
+                    print('age must be given as number of years alone (no "year" or "yr" and no decimals), please retry.')
 
-            #loops till valid Weight is given by user
-            while(self.Weight == 0):
+            #loops till valid weight is given by user
+            while(self.weight == 0):
                 #input
-                Attempt = input('Enter Weight in kgs (eg "34.5"):')
+                Attempt = input('Enter weight in kgs (eg "34.5"):')
                 #remove all whitespaces from back and front of input
                 Attempt = Attempt.strip()
 
                 #validates to see if input can be converted to an integer
                 try:
-                    self.Weight = float(Attempt)
-                    #if Weight is just a number and is above 0, it is valid, else prompt user to try again
-                    if self.Weight > 0:
+                    self.weight = float(Attempt)
+                    #if weight is just a number and is above 0, it is valid, else prompt user to try again
+                    if self.weight > 0:
                         break
                     else:
-                        print('Weight must be a positive number more than 0, please retry.')
+                        print('weight must be a positive number more than 0, please retry.')
                 #input contains more than just numbers prompt user to retry
                 except ValueError:
-                    print('Weight must be given as a number alone (no "kilograms" or "kgs")')
+                    print('weight must be given as a number alone (no "kilograms" or "kgs")')
 
-            #loops till valid Sex is given by user
-            while(self.Sex == ""):
+            #loops till valid sex is given by user
+            while(self.sex == ""):
                 #user input and input formatting
-                Attempt = input('Enter Sex(eg "M" or "F"):')
+                Attempt = input('Enter sex(eg "M" or "F"):')
                 Attempt = Attempt.strip().upper()
                 #checks if the input is M or F, if not warn user and allow it to be reentered
                 if Attempt in ["M", "F"]:
-                    self.Sex = Attempt
+                    self.sex = Attempt
                 else:
-                    print("Invalid Sex entered, for the Safety of other athletes either M or F needs to be specified for proper weight classification, please enter M or F")
+                    print("Invalid sex entered, for the Safety of other athletes either M or F needs to be specified for proper weight classification, please enter M or F")
 
             #loops till valid gym plan is given by user
             while(self.gym_plan == ""):
@@ -150,35 +150,35 @@ class Athlete(Person):
                 else:
                     print("Invalid number of hours selected .")
 
-        #assigns Weight catagory based on weight and sex
+        #assigns weight catagory based on weight and sex
         #Males
-        if self.Sex == "M" and self.Weight < 73:
-            self.Weight_category = "Male Light Weight"
+        if self.sex == "M" and self.weight < 73:
+            self.weight_category = "Male light weight"
         
-        elif self.Sex == "M" and self.Weight < 90:
-            self.Weight_category = "Male medium Weight"
+        elif self.sex == "M" and self.weight < 90:
+            self.weight_category = "Male medium weight"
         
-        elif self.Sex == "M" and self.Weight > 90:
-            self.Weight_category = "Male Heavy Weight"
+        elif self.sex == "M" and self.weight > 90:
+            self.weight_category = "Male heavy weight"
         #Females
-        elif self.Sex == "F" and self.Weight < 57:
-            self.Weight_category = "Female Light Weight"
+        elif self.sex == "F" and self.weight < 57:
+            self.weight_category = "Female light weight"
         
-        elif self.Sex == "F" and self.Weight < 70:
-            self.Weight_category = "Female medium Weight"
+        elif self.sex == "F" and self.weight < 70:
+            self.weight_category = "Female medium weight"
         
-        elif self.Sex == "F" and self.Weight > 70:
-            self.Weight_category = "Female Heavy Weight"
+        elif self.sex == "F" and self.weight > 70:
+            self.weight_category = "Female heavy weight"
         else:
             #this is impossible, but for whatever reason it does happen, it will be caught here
-            self.Weight_category = "unassigned"
-            print(f"warning: no parameters reached for placing in a weight catagory with sex: {self.Sex} and weight: {self.Weight}, please inform a member of staff.")
+            self.weight_category = "unassigned"
+            print(f"warning: no parameters reached for placing in a weight catagory with sex: {self.sex} and weight: {self.weight}, please inform a member of staff.")
 
         print("Athlete created successfully")
 
     def introduction(self):
         #little introduction function not needed or used
-        return f"Hello, my Name is {self.Name}, I am {self.Age} year old {self.get_Sex()} Athlete, and I Weight {self.Weight} kgs,\nthis puts me in the {self.Weight_category} category."
+        return f"Hello, my name is {self.name}, I am {self.age} year old {self.get_sex()} Athlete, and I weight {self.weight} kgs,\nthis puts me in the {self.weight_category} category."
     
     def get_plan(self):
         match self.gym_plan:
@@ -191,44 +191,44 @@ class Athlete(Person):
             case "4":
                 return "Professional"
             case _:
-                return f'unrecognised plan number "{self.gym_plan}" for athlete {self.Name}'
+                return f'unrecognised plan number "{self.gym_plan}" for athlete {self.name}'
 
     #print all the info of the athlete
     def print_info(self):
-        print(f"Name: {self.Name}")
-        print(f"Age: {self.Age}")
-        print(f"Weight: {self.Weight}")
-        print(f"Sex: {self.get_Sex()}")
-        print(f"Weight category: {self.Weight_category}")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Weight: {self.weight}")
+        print(f"Sex: {self.get_sex()}")
+        print(f"Weight category: {self.weight_category}")
         print(f"Gym plan: {self.get_plan()}")
         print(f"Competitions entered: {self.competitions}")
         print(f"Private coaching hours: {self.coaching_hours}")
         
 
 #returns the list of costs and total costs
-def calculate_cost(Athletes):
+def calculate_cost(athletes):
     #total price of all athletes plans
-    Price = 0
+    price = 0
     #indivdual prices in order of athletes
-    Prices = []
+    prices = []
     #loop through athletes and assign cost based on chosen plan
-    for instance in Athletes:
-        Plan_price = 0
+    for instance in athletes:
+        plan_price = 0
         
         #error catching tests to see if the gym plan number is recognised
         if instance.gym_plan in ["1", "2", "3", "4"]:
-            Plan_price = plans[instance.get_plan()]
+            plan_price = plans[instance.get_plan()]
         else:
             #There's no reason way a user can make an inccorect input but if they do somehow, its caught here
             print(instance.get_plan())
 
-        Plan_a_services = Plan_price+(competition_price*int(instance.competitions))+(private_coaching_price*int(instance.coaching_hours))
+        plan_and_services = plan_price+(competition_price*int(instance.competitions))+(private_coaching_price*int(instance.coaching_hours))
         #increase total cost
-        Price += Plan_a_services
+        price += plan_and_services
         #add cost to list
-        Prices.append(Plan_a_services)
+        prices.append(plan_and_services)
 
-    return [Price, Prices]
+    return [price, prices]
 
 
 def main():
@@ -239,7 +239,7 @@ def main():
         print("##########JUDO APP##########\n")
         print("Please type a number for service")
         print("1: Register Athlete")
-        print("2: View Athletes")
+        print("2: View athletes")
         print("3: Delete Athlete")
         print("4: Exit")
         
@@ -254,24 +254,24 @@ def main():
             match choice[0]:
                 #create a new athlete
                 case "1":
-                    #takes user to create an athlete then assigns to Athletes array when finished
-                    Athletes.append(Athlete(None, None, None, None, None, None, None, True))
+                    #takes user to create an athlete then assigns to athletes array when finished
+                    athletes.append(Athlete(None, None, None, None, None, None, None, True))
                 #list all athletes
                 case "2":
                     #gets the cost for all athletes individually and all together 
-                    prices = calculate_cost(Athletes)
-                    for instance in Athletes:
-                        print(f"\nAthlete {Athletes.index(instance)+1}:")
+                    prices = calculate_cost(athletes)
+                    for instance in athletes:
+                        print(f"\nAthlete {athletes.index(instance)+1}:")
                         instance.print_info()
-                        print(f"-Charge: £{prices[1][Athletes.index(instance)]}/Month")
+                        print(f"-Charge: £{prices[1][athletes.index(instance)]}/Month")
                     print(f"\nTotal: £{prices[0]}/Month")
                     input("Press Enter to go back to menu...")
                 
                 #delete athlete
                 case "3":
                     #lists all athletes for user to see
-                    for instance in Athletes:
-                        print(f"\nAthlete {Athletes.index(instance)+1}:")
+                    for instance in athletes:
+                        print(f"\nAthlete {athletes.index(instance)+1}:")
                         instance.print_info()
                     #prompts user for choice and removes whitespace from sides
                     delete_Attempt = input("Enter the number of the Athlete you want to delete:")
@@ -282,9 +282,9 @@ def main():
                         #int conversion
                         delete_Attempt = int(delete_Attempt[0])
                         #check if input is above zero and less or equal to number of athletes
-                        if delete_Attempt > 0 and delete_Attempt <= len(Athletes):
+                        if delete_Attempt > 0 and delete_Attempt <= len(athletes):
                            #deletes athletes
-                            del Athletes[delete_Attempt-1]
+                            del athletes[delete_Attempt-1]
                             print("Athlete deleted successfully")
                         #warn if number is invalid
                         else:
